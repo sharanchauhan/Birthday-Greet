f44ee5bd50971cdc6b4709e4249349cd775ae2eb
sharanchauhan <sharanchauhan29@gmail.com>
2022-04-03 23:56:22 +0530

    Created a new activity and changed the build.gradle file in gradle scripts to make the id-input_name work properly

--- app/src/main/java/com/example/birthdaygreet/MainActivity.kt
+++ app/src/main/java/com/example/birthdaygreet/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.birthdaygreet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createBirthdayCard(view: View) {
+        val name=input_name.editableText.toString()
+        Toast.makeText(this, "Name is $name", Toast.LENGTH_LONG).show()
     }
 }
